64333b5323a04b3a5826278badf7e6db04ea1b48
saidalizada <saidalizada@gmail.com>
2023-08-03 17:55:48 +0200

    Added ordering

--- courses/models.py
+++ courses/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
-
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -35,8 +34,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 class Content(models.Model):
     module = models.ForeignKey(Module,
@@ -51,6 +55,10 @@ class Content(models.Model):
                                      'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,
