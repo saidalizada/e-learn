b4fe412921a23e0555c0adc12a071270e7d85e32
saidalizada <saidalizada@gmail.com>
2023-08-07 10:20:43 +0200

    Added display courses

--- elearn/urls.py
+++ elearn/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
+
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), 
@@ -27,6 +29,7 @@ urlpatterns = [
           name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
