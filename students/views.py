d023d8ea47e293162e1ca79394059a4504330660
saidalizada <saidalizada@gmail.com>
2023-08-07 10:37:15 +0200

    Added Enroll Now

--- students/views.py
+++ students/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CourseEnrollForm
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
     form_class = UserCreationForm
@@ -13,3 +15,15 @@ class StudentRegistrationView(CreateView):
                             password=cd['password1'])
         login(self.request, user)
         return result
+
+class StudentEnrollCourseView(LoginRequiredMixin, 
+                              FormView):
+    course = None
+    form_class = CourseEnrollForm
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail',
+                            args=[self.course.id])
